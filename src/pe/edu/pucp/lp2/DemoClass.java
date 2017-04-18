9871386a8b28594174bf4445f85f5ce3026772fa
johanpbm <johan.baldeon@pucp.edu.pe>
2017-04-18 12:15:30 +0200

    First commit.

---
+++ src/pe/edu/pucp/lp2/DemoClass.java
@@ -0,0 +1,10 @@
+package pe.edu.pucp.lp2;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo GitHub");
+	}
+
+}
